a68304712450b90cdcf3b6840d5e08030a62d176
software-forge <softforge17@gmail.com>
2017-11-22 22:31:46 +0100

    Zastosowano klasę Komunikat() TODO: 1. zobaczyć czy jeszcze gdzieś można ją zastosować i to zrobić 2. dodać tytuły menu 3. dodać zrzut floty do xml

--- Wypozyczalnia/Flota.cs
+++ Wypozyczalnia/Flota.cs
@@ -33,6 +33,8 @@ namespace Wypozyczalnia
             Console.Write("Cena: ");
             s.Cena = Convert.ToDecimal(Console.ReadLine());
             samochody.Add(s);
+            Komunikat k = new Komunikat("Dodano");
+            k.Wyswietl();
         }
 
         /// <summary>
@@ -40,10 +42,15 @@ namespace Wypozyczalnia
         /// </summary>
         public void Usun()
         {
+            // Można przebudować sensowniej
             if(samochody.Count() > 0)
             {
                 while (true)
                 {
+                    // Jeżeli użytkownik usunął wszystkie samochody, to powrót do głównego menu !!!
+                    if (samochody.Count() == 0)
+                        break;
+
                     Menu listaAut = new Menu(samochody.Count() + 1);
                     foreach (Samochod s in samochody)
                     {
@@ -77,6 +84,7 @@ namespace Wypozyczalnia
             else
             {
                 Komunikat k = new Komunikat("-- Brak samochodów --");
+                k.Wyswietl();
             }
         }
 
